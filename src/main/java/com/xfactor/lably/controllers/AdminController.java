c7d0352f4f7810e9517bc31ddbb6cead26d186cf
Siddhartha Banerjee <siddhartha754@gmail.com>
2021-05-02 20:19:01 +0530

    added controllers

---
+++ src/main/java/com/xfactor/lably/controllers/AdminController.java
@@ -0,0 +1,54 @@
+package com.xfactor.lably.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.xfactor.lably.entity.Admin;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    ArrayList<Admin> admins = new ArrayList<>();
+
+   
+    @GetMapping("/getAdmin")
+    public ArrayList<Admin> getAdmin() {
+        return admins;
+    }
+
+    @PostMapping("/addAdmin")
+    public Admin addAdmin(@RequestBody Admin admin) {
+
+        admins.add(admin);
+        return admin;
+    }
+
+    @GetMapping("/search")
+    public ArrayList<Admin> searchAdmin(@RequestParam String username){
+
+        ArrayList<Admin> ad = new ArrayList<>();
+        boolean f = false;
+
+        for(Admin a: admins){
+            if(a.getUsername().equals(username)){
+                f = true;
+                ad.add(a);
+            }              
+        }
+        if(f==true) return ad;
+        else return null;
+    }
+
+}
---
+++ src/main/java/com/xfactor/lably/controllers/LabController.java
@@ -0,0 +1,52 @@
+package com.xfactor.lably.controllers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+import com.xfactor.lably.entity.Lab;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lab")
+public class LabController {
+
+    ArrayList<Lab> labs = new ArrayList<>();
+
+    @GetMapping("/getLab")
+    public ArrayList<Lab> getAdmin() {
+        return labs;
+    }
+
+    @PostMapping("/addLab")
+    public Lab addLab(@RequestBody Lab lab) {
+
+        labs.add(lab);
+        return lab;
+    }
+
+    @GetMapping("/search")
+    public ArrayList<Lab> searchAdmin(@RequestParam String name){
+
+        ArrayList<Lab> ad = new ArrayList<>();
+        boolean f = false;
+
+        for(Lab a: labs){
+            if(a.getName().equals(name)){
+                f = true;
+                ad.add(a);
+            }              
+        }
+        if(f==true) return ad;
+        else return null;
+    }
+}
